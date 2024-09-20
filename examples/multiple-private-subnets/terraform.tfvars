464400f5715477e08cc0b2143999dc8d83bc88c1
Matteo <97225998+matteo-ssys@users.noreply.github.com>
2024-09-20 17:18:29 +0000

    Example to deploy multiple private subnets

---
+++ examples/multiple-private-subnets/terraform.tfvars
@@ -0,0 +1,10 @@
+
+region = "us-east-1"
+
+availability_zones = ["us-east-1a", "us-east-1b"]
+
+namespace = "eg"
+
+stage = "test"
+
+name = "subnets-vpc-test"
---
+++ examples/multiple-private-subnets/variables.tf
@@ -0,0 +1,10 @@
+variable "region" {
+  type        = string
+  description = "AWS region"
+}
+
+variable "availability_zones" {
+  type        = list(string)
+  description = "List of Availability Zones where subnets will be created"
+}
+
